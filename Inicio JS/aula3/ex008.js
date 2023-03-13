4105f9adddc931142a8cb3c3f765598d460a252a
Euflausino <113407109+EduardoHantrir@users.noreply.github.com>
2023-03-13 19:28:50 -0300

    Upload das Aulas

---
+++ Inicio JS/aula3/ex008.js
@@ -0,0 +1,11 @@
+//Aula de JS - Condições
+
+var vel = 60
+
+console.log(`Sua velocidade é de ${vel}Km`)
+if (vel <= 60){
+    console.log('Dirija sempre com cuidado')
+}
+else{
+    console.log('Você esta acima da velocidade limite, multa')
+}
